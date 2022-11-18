c3d5b6fd5d33ba2f6dbe7463a6a2e4afc8c3af06
Darkness__31 <69047879+darkness-31@users.noreply.github.com>
2022-11-19 02:55:01 +0300

    я не смог

--- Arenda_Samokatov/Program.cs
+++ Arenda_Samokatov/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
@@ -28,7 +29,6 @@ internal class Program
         do
         {
             Console.Clear();
-            Console.WriteLine();
             Console.WriteLine("1. Добавить клиентов");
             Console.WriteLine("2. Добавить самокатов");
             Console.WriteLine("3. Вывод списка клиентов");
@@ -69,14 +69,20 @@ internal class Program
                     ViewClients();
                     break;
                 case 4:
+                    ViewScooter();
                     break;
                 case 5:
+                    AddRent();
                     break;
                 case 6:
+                    ViewRent();
                     break;
                 case 7:
                     break;
                 case 8:
+                    if (!SuperUser)
+                        continue;
+                    EditClient();
                     break;
                 case 9:
                     break;
@@ -196,9 +202,185 @@ internal class Program
                 break;
         }
 
-        string sql = "INSERT INTO scooter " +
+        string sql = "INSERT INTO scooters (name) " +
                      $"VALUES ('{name}')";
         sql.SQLNonQuery();
+    }
+    private static void AddRent()
+    {
+        int client = 1, scooter = 1;
+        while (true)
+        {
+            string sql = "SELECT COUNT(*) " +
+                         "FROM clients";
+            int count = sql.SQLQueryAsList<int>().First();
+
+            ConsoleKeyInfo key;
+            bool checkWhile = true;
+            int pageStart = 1;
+            int pageEnd = 9;
+            while (checkWhile)
+            {
+                sql = "SELECT id, " +
+                             "name, " +
+                             "surname, " +
+                             "phone_number " +
+                      "FROM clients " +
+                     $"WHERE id >= {pageStart} and " +
+                           $"id <= {pageEnd}";
+                List<string> list = sql.SQLQueryAsList<string>();
+
+                Console.Clear();
+                GridView(list, "Номер", "Имя", "Фамилия", "Номер телефона");
+
+                Console.WriteLine();
+                Console.WriteLine("E - Ввести номер клиента\nB - Предыдущая страница\nN - Следующая страница\nQ - Выйти");
+
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.B &&
+                    pageStart != 1)
+                {
+                    pageStart -= 9;
+                    pageEnd -= 9;
+                }
+                else if (key.Key == ConsoleKey.N &&
+                    pageEnd <= count)
+                {
+                    pageStart += 9;
+                    pageEnd += 9;
+                }
+                else if (key.Key == ConsoleKey.E)
+                {
+                    Console.Write("Номер: ");
+                    client = Console.ReadLine().Parse<int>();
+                    break;
+                }
+                else if (key.Key == ConsoleKey.Q)
+                    return;
+            }
+
+            sql = "SELECT COUNT(*) " +
+                         "FROM scooters";
+            count = sql.SQLQueryAsList<int>().First();
+
+            checkWhile = true;
+            pageStart = 1;
+            pageEnd = 9;
+            while (checkWhile)
+            {
+                sql = "SELECT id, " +
+                             "name " +
+                      "FROM scooters " +
+                     $"WHERE id >= {pageStart} and " +
+                           $"id <= {pageEnd}";
+                List<string> list = sql.SQLQueryAsList<string>();
+
+                Console.Clear();
+                GridView(list, "Номер", "Название");
+
+                Console.WriteLine();
+                Console.WriteLine("E - Ввести номер клиента\nB - Предыдущая страница\nN - Следующая страница\nQ - Выйти");
+
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.B &&
+                    pageStart != 1)
+                {
+                    pageStart -= 9;
+                    pageEnd -= 9;
+                }
+                else if (key.Key == ConsoleKey.N &&
+                    pageEnd <= count)
+                {
+                    pageStart += 9;
+                    pageEnd += 9;
+                }
+                else if (key.Key == ConsoleKey.E)
+                {
+                    Console.Write("Номер: ");
+                    scooter = Console.ReadLine().Parse<int>();
+                    break;
+                }
+                else if (key.Key == ConsoleKey.Q)
+                    return;
+            }
+
+            Console.Clear();
+
+            sql = "SELECT clients.name, " +
+                         "clients.surname, " +
+                         "scooters.name " +
+                  "FROM clients LEFT JOIN " +
+                        $"scooters on scooters.id = {scooter} " +
+                 $"WHERE clients.id = {client}";
+            var _list = sql.SQLQueryAsList<string>();
+            _list.Add(DateTime.Now.ToString());
+            GridView(_list, "Имя", "Фамилия", "Скутер", "Время");
+            Console.WriteLine("Правильно? [д/н] ");
+            var cheeck = Console.ReadLine();
+            if (cheeck?.ToLower() == "y" ||
+                cheeck?.ToLower() == "д")
+            {
+                sql = "INSERT INTO rent (client_id, scooter_id, date) " +
+                     $"VALUES ({client},{scooter},datetime('now'))";
+                sql.SQLNonQuery();
+            }
+            else
+                Console.Clear();
+        }
+    }
+
+    private static void EditClient()
+    {
+        string sql = "SELECT COUNT(*) " +
+                     "FROM clients";
+        int count = sql.SQLQueryAsList<int>().First();
+
+        ConsoleKeyInfo key;
+        bool checkWhile = true;
+        int pageStart = 1;
+        int pageEnd = 9;
+        while (checkWhile)
+        {
+
+            sql = "SELECT id, " +
+                         "name, " +
+                         "surname, " +
+                         "phone_number " +
+                  "FROM clients " +
+                 $"WHERE id >= {pageStart} and " +
+                       $"id <= {pageEnd}";
+            List<string> list = sql.SQLQueryAsList<string>();
+
+            Console.Clear();
+            GridView(list, "Номер", "Имя", "Фамилия", "Номер телефона");
+
+            Console.WriteLine();
+            Console.WriteLine("E - Ввести номер\nB - Предыдущая страница\nN - Следующая страница\nQ - Выйти");
+
+            key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.B &&
+                pageStart != 1)
+            {
+                pageStart -= 9;
+                pageEnd -= 9;
+            }
+            if (key.Key == ConsoleKey.E)
+            {
+                Console.WriteLine();
+                Console.Write("Имя: ");
+            }
+            else if (key.Key == ConsoleKey.N &&
+                pageEnd <= count)
+            {
+                pageStart += 9;
+                pageEnd += 9;
+            }
+            else if (key.Key == ConsoleKey.Q)
+                break;
+        }
     }
 
     private static void ViewClients()
@@ -208,12 +390,14 @@ internal class Program
         int count = sql.SQLQueryAsList<int>().First();
 
         ConsoleKeyInfo key;
-        while ((key = Console.ReadKey(true)).Key != ConsoleKey.Q)
+        bool checkWhile = true;
+        int pageStart = 1;
+        int pageEnd = 9;
+        while (checkWhile)
         {
-            int pageStart = 1;
-            int pageEnd = 9;
 
-            sql = "SELECT name, " +
+            sql = "SELECT id, " +
+                         "name, " +
                          "surname, " +
                          "phone_number " +
                   "FROM clients " +
@@ -222,7 +406,12 @@ internal class Program
             List<string> list = sql.SQLQueryAsList<string>();
 
             Console.Clear();
-            GridView(list, "name", "surname", "Phone Number");
+            GridView(list, "Номер", "Имя", "Фамилия", "Номер телефона");
+
+            Console.WriteLine();
+            Console.WriteLine("B - предыдущая страница\tQ - Выйти\tN - Следующая страница");
+
+            key = Console.ReadKey(true);
 
             if (key.Key == ConsoleKey.B &&
                 pageStart != 1)
@@ -231,7 +420,7 @@ internal class Program
                 pageEnd -= 9;
             }
             else if (key.Key == ConsoleKey.N &&
-                pageEnd != count)
+                pageEnd <= count)
             {
                 pageStart += 9;
                 pageEnd += 9;
@@ -240,20 +429,119 @@ internal class Program
                 break;
         }
     }
+    private static void ViewScooter()
+    {
+        string sql = "SELECT COUNT(*) " +
+                             "FROM scooters";
+        int count = sql.SQLQueryAsList<int>().First();
+
+        ConsoleKeyInfo key;
+        bool checkWhile = true;
+        int pageStart = 1;
+        int pageEnd = 9;
+        while (checkWhile)
+        {
+
+            sql = "SELECT id, " +
+                         "name " +
+                  "FROM scooters " +
+                 $"WHERE id >= {pageStart} and " +
+                       $"id <= {pageEnd}";
+            List<string> list = sql.SQLQueryAsList<string>();
+
+            Console.Clear();
+            GridView(list, "Номер", "Название");
+            Console.WriteLine();
+            Console.WriteLine("B - предыдущая страница\tQ - Выйти\tN - Следующая страница");
+
+            key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.B &&
+                pageStart != 1)
+            {
+                pageStart -= 9;
+                pageEnd -= 9;
+            }
+            else if (key.Key == ConsoleKey.N &&
+                pageEnd <= count)
+            {
+                pageStart += 9;
+                pageEnd += 9;
+            }
+            else if (key.Key == ConsoleKey.Q)
+                break;
+        }
+    }
+    private static void ViewRent()
+    {
+
+        string sql = "SELECT COUNT(*) " +
+                     "FROM rent";
+        int count = sql.SQLQueryAsList<int>().First();
+
+        ConsoleKeyInfo key;
+        bool checkWhile = true;
+        int pageStart = 1;
+        int pageEnd = 9;
+        while (checkWhile)
+        {
+
+            sql = "SELECT clients.name, " +
+                         "clients.surname, " +
+                         "clients.phone_number, " +
+                         "scooter.name, " +
+                         "rent.date" +
+                  "FROM rent INNER JOIN " +
+                         "clients ON rent.client_id = clients.id INNER JOIN " +
+                         "scooters ON rent.scooter_id = scooters.id" +
+                 $"WHERE id >= {pageStart} and " +
+                       $"id <= {pageEnd}";
+            List<string> list = sql.SQLQueryAsList<string>();
+
+            Console.Clear();
+            GridView(list, "Имя", "Фамилия", "Номер телефона", "Самокат", "Дата");
+
+            Console.WriteLine();
+            Console.WriteLine("B - предыдущая страница\tQ - Выйти\tN - Следующая страница");
+
+            key = Console.ReadKey(true);
+
+            if (key.Key == ConsoleKey.B &&
+                pageStart != 1)
+            {
+                pageStart -= 9;
+                pageEnd -= 9;
+            }
+            else if (key.Key == ConsoleKey.N &&
+                pageEnd <= count)
+            {
+                pageStart += 9;
+                pageEnd += 9;
+            }
+            else if (key.Key == ConsoleKey.Q)
+                break;
+        }
+    }
+
     private static void GridView(List<string> list, params string[] columnsName)
     {
+        int[] maxCount = new int[columnsName.Length];
+        for (int i = 0; i < list.Count; i += columnsName.Length)
+            for (int a = 0; a < columnsName.Length; a++)
+                maxCount[a] = list[a + i].Length > maxCount[a] ? list[a + i].Length : maxCount[a];
+
         for (int i = 0; i < columnsName.Count(); i++)
         {
-            var num = columnsName[i].Length / 4 > 2 ? 2 : 1;
-            string tab = string.Join("", Enumerable.Repeat("\t", num));
-            Console.Write(columnsName[i] + tab);
+            var num = tabCount(maxCount[i], columnsName[i].Length);
+            string str = columnsName[i] + string.Join("", Enumerable.Repeat("\t", num));
+            Console.Write(str);
         }
+
         Console.WriteLine();
         for (int i = 0; i < columnsName.Count(); i++)
         {
-            string tire = string.Join("", Enumerable.Repeat("-", columnsName[i].Length + 1));
-            var num = tire.Length / 4 > 2 ? 2 : 1;
-            string tab = string.Join("", Enumerable.Repeat("\t", num));
+            string tire = string.Join("", Enumerable.Repeat("-", columnsName[i].Length)); 
+            string tab = string.Join("", Enumerable.Repeat("\t", tabCount(maxCount[i], columnsName[i].Length)));
             Console.Write(tire + tab);
         }
 
@@ -262,10 +550,29 @@ internal class Program
         {
             for (int a = 0; a < columnsName.Length; a++)
             {
-
-                Console.Write(list[a+i] + string.Join("", Enumerable.Repeat("\t", counts[a])));
+                var num = tabCount(maxCount[a], list[a+i].Length);
+                Console.Write(list[a+i] + string.Join("", Enumerable.Repeat("\t", num)));
             }
             Console.WriteLine();
         }
+    }
+    private static int tabCount(int max, int value) 
+    {
+        int stat = 0;
+        var temp = (max + (4 - max % 4)) - value;
+        while (true)
+        {
+            if (temp <= 4)
+            {
+                ++stat;
+                break;
+            }
+            else
+            {
+                ++stat;
+                temp -= 4;
+            }
+        }
+        return stat;
     }
 }
